c899058b519175c191ab48e2b0fc459922a9fa42
wzfgj0001 <git@github.com>
2017-03-15 16:45:40 +0800

    测试代码提交

---
+++ app/src/main/java/servicetest/zkk/com/myapplication/CanvasExa/TestGitActivity.java
@@ -0,0 +1,13 @@
+package servicetest.zkk.com.myapplication.CanvasExa;
+
+import android.app.Activity;
+
+/**
+ * Function:<br>
+ * <p/>
+ * date: 2017/3/15.
+ *
+ * @author GuJian
+ */
+public class TestGitActivity extends Activity{
+}
